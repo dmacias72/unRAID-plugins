fadff0848a9cb26412796ab8223c73e6f092dd77
Derek Macias <derekmacias@live.com>
2016-01-15 01:43:30 -0700

    update

--- source/speedtest/install/doinst.sh
+++ source/speedtest/install/doinst.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-xml /usr/sbin/speedtest-xml
 chmod +0755 /usr/sbin/speedtest-xml
+
+ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-linux /usr/sbin/speedtest-linux
+chmod +0755 /usr/sbin/speedtest-linux
