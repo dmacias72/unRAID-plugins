6bb55075209332064672d67a8f751cbc6f87119d
dmacias72 <derekmacias@live.com>
2016-02-28 02:13:09 -0700

    update command for server list

--- source/speedtest/usr/local/emhttp/plugins/speedtest/include/speedtest-list.php
+++ source/speedtest/usr/local/emhttp/plugins/speedtest/include/speedtest-list.php
@@ -1,9 +1,9 @@
 <?php
-$command = "speedtest --list 2>/dev/null";
+$command = '/usr/bin/python /usr/local/emhttp/plugins/speedtest/scripts/speedtest.py --list 2>/dev/null';
 exec($command, $output);
 $array = array();
 for ($i = 2; $i < sizeof($output); $i++) {
-	$value = explode(") ", $output[$i],2);
+	$value = explode(') ', $output[$i],2);
 	$array[] = $value;
 }
 echo json_encode($array);
