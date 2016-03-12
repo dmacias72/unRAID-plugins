b53313924562cdae8d561a4adbd26204805a310a
dmacias72 <derekmacias@live.com>
2016-03-12 08:54:32 -0700

    move permissions and scripts to package install

--- source/libvirtwol/install/doinst.sh
+++ source/libvirtwol/install/doinst.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+SCRIPT="<?php include_once '/usr/local/emhttp/plugins/libvirtwol/include/libvirtwol.php';?>"
+FILE="/usr/local/emhttp/plugins/dynamix.vm.manager/VMSettings.page"
+
+# Update file permissions of scripts
+chmod +0755 /usr/local/emhttp/plugins/libvirtwol/scripts/* \
+	/usr/local/emhttp/plugins/libvirtwol/event/*
+
+# add stop to shutdown script	
+if ! grep "$SCRIPT" $FILE >/dev/null 2>&1
+	then echo -e "\n$SCRIPT" >> $FILE
+fi
+
+
