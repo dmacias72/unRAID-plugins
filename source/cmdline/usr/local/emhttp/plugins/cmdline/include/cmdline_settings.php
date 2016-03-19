003936852f073656e6d5e6bd48d2d95bedc5dcc3
Derek Macias <derekmacias@live.com>
2016-03-19 06:41:19 -0600

    fix runas user and port status

--- source/cmdline/usr/local/emhttp/plugins/cmdline/include/cmdline_settings.php
+++ source/cmdline/usr/local/emhttp/plugins/cmdline/include/cmdline_settings.php
@@ -10,6 +10,6 @@ $shellinabox_cert = isset($cmdline_cfg['CERT'])			? $cmdline_cfg['CERT']		:  "ce
 $shellinabox_version = shell_exec( "/usr/sbin/shellinaboxd --version 2>&1 | grep ShellInABox | sed -e 's/^ShellInABox version //;s/(.*//'" );
 $shellinabox_port_status = ($shellinabox_running) ? 
 	"<a target='_blank' href='http://".$shellinabox_host.":".$shellinabox_port."' title='Click on link then accept security, then /Tools/CommandLine will work'><b><font color='green'>$shellinabox_port</font></b></a>":
-	"<b><font color='orange';'>$shellinabox_port</font></b>";
+	"<b><font color='orange'>$shellinabox_port</font></b>";
 exec("awk -F':' '{ if ( $3 >= 1000 ) print $1}' /etc/passwd", $shellinabox_users); // get array of group users
 ?>
