0dd8b3bce5d6b37b07c9fb8b30d8b0a28a09d8fe
dmacias72 <derekmacias@live.com>
2016-04-01 20:07:45 -0600

    add fan poll option

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php
@@ -8,13 +8,14 @@ if (is_file($fan_cfg_file))
 	$fan_cfg	= parse_ini_file($fan_cfg_file);
 
 /* fan control */
-$ipmifan						= isset($fan_cfg['IPMIFAN'])			? $fan_cfg['IPMIFAN']			: "disable";
+$ipmifan       = isset($fan_cfg['IPMIFAN'])   ? $fan_cfg['IPMIFAN']   : "disable";
+$fanpoll      = isset($fan_cfg['FANPOLL'])  ? $fan_cfg['FANPOLL']  : 1;
 
 /* ipmi network options */
-$ipmi_network		= isset($ipmi_cfg['NETWORK'])		? $ipmi_cfg['NETWORK']		: 'disable';
-$ipmi_ipaddr			= isset($ipmi_cfg['IPADDR'])			? $ipmi_cfg['IPADDR']			: '';
-$ipmi_user				= isset($ipmi_cfg['USER'])					? $ipmi_cfg['USER']				: '';
-$ipmi_password	= isset($ipmi_cfg['PASSWORD'])	? $ipmi_cfg['PASSWORD']	: '';
+$ipmi_network  = isset($ipmi_cfg['NETWORK'])  ? $ipmi_cfg['NETWORK']  : 'disable';
+$ipmi_ipaddr   = isset($ipmi_cfg['IPADDR'])   ? $ipmi_cfg['IPADDR']   : '';
+$ipmi_user     = isset($ipmi_cfg['USER'])     ? $ipmi_cfg['USER']     : '';
+$ipmi_password	= isset($ipmi_cfg['PASSWORD']) ? $ipmi_cfg['PASSWORD'] : '';
 
 /* check if local ipmi driver is loaded */
 $ipmi_mod = shell_exec("modprobe ipmi_si --first-time 2>&1 | grep -q 'Module already in kernel' && echo 1 || echo 0 2> /dev/null");
