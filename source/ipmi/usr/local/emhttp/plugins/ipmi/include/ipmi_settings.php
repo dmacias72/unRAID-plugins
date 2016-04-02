b9f20142a0d87c99fd4d07d77a46d4bc937a6582
dmacias72 <derekmacias@live.com>
2016-04-01 20:07:58 -0600

    update format

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_settings.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_settings.php
@@ -2,9 +2,9 @@
 require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_helpers.php';
 
 /* ipmi settings variables*/
-$ipmiseld		= isset($ipmi_cfg['IPMISELD']) ? $ipmi_cfg['IPMISELD']	: "disable";
-$ipmipoll		= isset($ipmi_cfg['IPMIPOLL']) ? $ipmi_cfg['IPMIPOLL']	: "60";
-$ipmi_local		= isset($ipmi_cfg['LOCAL'])	 ? $ipmi_cfg['LOCAL']		: "disable";
+$ipmiseld   = isset($ipmi_cfg['IPMISELD']) ? $ipmi_cfg['IPMISELD'] : "disable";
+$ipmipoll   = isset($ipmi_cfg['IPMIPOLL']) ? $ipmi_cfg['IPMIPOLL'] : "60";
+$ipmi_local = isset($ipmi_cfg['LOCAL'])    ? $ipmi_cfg['LOCAL']    : "disable";
 
 //check running status
 $ipmiseld_running = trim(shell_exec( "[ -f /proc/`cat /var/run/ipmiseld.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ));
@@ -16,15 +16,15 @@ $ipmifan_status = ($ipmifan_running) ? $ipmi_running : $ipmi_stopped;
 
 /* get display temps and fans */
 $ipmi_disp_temp1 = isset($ipmi_cfg['DISP_TEMP1']) ? $ipmi_cfg['DISP_TEMP1'] : "";
-$ipmi_disp_temp2  = isset($ipmi_cfg['DISP_TEMP2'])  ? $ipmi_cfg['DISP_TEMP2']  : "";
-$ipmi_disp_fan1 = isset($ipmi_cfg['DISP_FAN1']) ? $ipmi_cfg['DISP_FAN1'] : "";
-$ipmi_disp_fan2 = isset($ipmi_cfg['DISP_FAN2']) ? $ipmi_cfg['DISP_FAN2'] : "";
+$ipmi_disp_temp2 = isset($ipmi_cfg['DISP_TEMP2']) ? $ipmi_cfg['DISP_TEMP2'] : "";
+$ipmi_disp_fan1  = isset($ipmi_cfg['DISP_FAN1'])  ? $ipmi_cfg['DISP_FAN1']  : "";
+$ipmi_disp_fan2  = isset($ipmi_cfg['DISP_FAN2'])  ? $ipmi_cfg['DISP_FAN2']  : "";
 
 /* Get sensor info and check connection */
 if($ipmi_mod || $ipmi_network == 'enable') {
 	$ipmi_sensors = ipmi_sensors($ipmi_options);
-	$ipmi_fans = ipmi_get_fans($ipmi_sensors);
-	$ipmi_board = trim(shell_exec("ipmi-fru $ipmi_options | grep 'Board Manufacturer' | awk -F ':' '{print $2}'")); // motherboard
+	$ipmi_fans    = ipmi_get_fans($ipmi_sensors);
+	$ipmi_board   = trim(shell_exec("ipmi-fru $ipmi_options | grep 'Board Manufacturer' | awk -F ':' '{print $2}'")); // motherboard
 }
 if($ipmi_network == 'enable'){
 	$ipmi_conn = ($ipmi_sensors) ? "Connection successful" : "Connection failed";
