49c8077f0a6dfd066d9142f6f4cd39e29ee22805
dmacias72 <derekmacias@live.com>
2016-04-03 00:16:52 -0600

    formatting

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_settings.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_settings.php
@@ -7,12 +7,12 @@ $ipmipoll   = isset($ipmi_cfg['IPMIPOLL']) ? $ipmi_cfg['IPMIPOLL'] : "60";
 $ipmi_local = isset($ipmi_cfg['LOCAL'])    ? $ipmi_cfg['LOCAL']    : "disable";
 
 //check running status
-$ipmiseld_running = trim(shell_exec( "[ -f /proc/`cat /var/run/ipmiseld.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ));
-$ipmifan_running = trim(shell_exec( "[ -f /proc/`cat /var/run/ipmifan.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ));
+$ipmiseld_running   = trim(shell_exec( "[ -f /proc/`cat /var/run/ipmiseld.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ));
+$fancontrol_running = trim(shell_exec( "[ -f /proc/`cat /var/run/ipmifan.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ));
 $ipmi_running = "<span class='green'>Running</span>";
 $ipmi_stopped = "<span class='orange'>Stopped</span>";
-$ipmiseld_status = ($ipmiseld_running) ? $ipmi_running : $ipmi_stopped;
-$ipmifan_status = ($ipmifan_running) ? $ipmi_running : $ipmi_stopped;
+$ipmiseld_status   = ($ipmiseld_running)   ? $ipmi_running : $ipmi_stopped;
+$fancontrol_status = ($fancontrol_running) ? $ipmi_running : $ipmi_stopped;
 
 /* get display temps and fans */
 $ipmi_disp_temp1 = isset($ipmi_cfg['DISP_TEMP1']) ? $ipmi_cfg['DISP_TEMP1'] : "";
