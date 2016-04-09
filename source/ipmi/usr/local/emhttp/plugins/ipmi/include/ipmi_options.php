538b52c2d11e0a49fcb1ada707cbb8017db0071a
dmacias72 <derekmacias@live.com>
2016-04-09 15:40:22 -0600

    check for network disabled for  ipmi modprobe

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php
@@ -7,18 +7,20 @@ if (is_file($ipmi_cfg_file))
 if (is_file($fan_cfg_file))
 	$fan_cfg    = parse_ini_file($fan_cfg_file);
 
-/* fan control */
-$fancontrol    = isset($fan_cfg['FANCONTROL']) ? $fan_cfg['FANCONTROL'] : "disable";
-$fanpoll       = isset($fan_cfg['FANPOLL'])    ? $fan_cfg['FANPOLL']    : 1;
-
 /* ipmi network options */
 $ipmi_network  = isset($ipmi_cfg['NETWORK'])   ? $ipmi_cfg['NETWORK']   : 'disable';
 $ipmi_ipaddr   = isset($ipmi_cfg['IPADDR'])    ? $ipmi_cfg['IPADDR']    : '';
 $ipmi_user     = isset($ipmi_cfg['USER'])      ? $ipmi_cfg['USER']      : '';
 $ipmi_password	= isset($ipmi_cfg['PASSWORD'])  ? $ipmi_cfg['PASSWORD']  : '';
 
+/* fan control */
+$fancontrol    = isset($fan_cfg['FANCONTROL']) ? $fan_cfg['FANCONTROL'] : "disable";
+$fanpoll       = isset($fan_cfg['FANPOLL'])    ? $fan_cfg['FANPOLL']    : 1;
+$fanip = isset($fan_cfg['FANIP'])    ? $fan_cfg['FANIP']    : 'None';
+
 /* check if local ipmi driver is loaded */
-$ipmi_mod = shell_exec("modprobe ipmi_si --first-time 2>&1 | grep -q 'Module already in kernel' && echo 1 || echo 0 2> /dev/null");
+if($ipmi_network == "disable")
+	$ipmi_mod = shell_exec("modprobe ipmi_si --first-time 2>&1 | grep -q 'Module already in kernel' && echo 1 || echo 0 2> /dev/null");
 
 /* options for network access */
 $ipmi_options = ($ipmi_network == 'enable') ? "--always-prefix -h $ipmi_ipaddr -u $ipmi_user -p ".
