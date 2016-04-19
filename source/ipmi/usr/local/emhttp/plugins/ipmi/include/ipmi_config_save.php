2975b57b2ad509b7b090bce040e8eac2bf374b6e
dmacias72 <derekmacias@live.com>
2016-04-18 22:09:31 -0600

    exec

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_config_save.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_config_save.php
@@ -6,7 +6,7 @@ if(array_key_exists('commit', $_POST) && !empty($_POST['ipmicfg'])){
 	$config_file = "$plg_path/ipmi.config";
 	file_put_contents($config_file, $_POST['ipmicfg']);
 	$cmd = "ipmi-sensors-config --diff $netopts 2>&1";//--filename=$config_file --commit $netopts";
-	//exec($cmd, $output, $return);
+	exec($cmd, $output, $return);
 }
 if($return)
 	$return = ['error' => $output];
