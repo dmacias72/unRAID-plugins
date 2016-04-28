93416e33188313d04742bf235c485366b12b57a2
dmacias72 <derekmacias@live.com>
2016-04-27 21:57:21 -0600

    remove CR from config

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_config_save.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_config_save.php
@@ -3,8 +3,9 @@ require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php';
 
 $return = false;
 if(array_key_exists('commit', $_POST) && !empty($_POST['ipmicfg'])){
+	$data = str_replace("\r", '', $_POST['ipmicfg']);
 	$config_file = "$plg_path/ipmi.config";
-	file_put_contents($config_file, $_POST['ipmicfg']);
+	file_put_contents($config_file, $data);
 	$cmd = "ipmi-sensors-config --diff $netopts 2>&1";//--filename=$config_file --commit $netopts";
 	exec($cmd, $output, $return);
 }
