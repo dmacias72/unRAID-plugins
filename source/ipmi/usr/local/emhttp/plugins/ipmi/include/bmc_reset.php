c4f497705455ad980066ed53ee4777941a7cdca2
dmacias72 <derekmacias@live.com>
2016-05-01 17:50:35 -0600

    add error reporting

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/bmc_reset.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/bmc_reset.php
@@ -1,5 +1,12 @@
 <?
 require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php';
 $cmd = "bmc-device --warm-reset $netopts";
-shell_exec($cmd);
+exec($cmd, $output, $return_var=null);
+
+if($return_var)
+	$return = ['error' => $output];
+else
+	$return = ['success' => true];
+
+echo json_encode($return);
 ?>
