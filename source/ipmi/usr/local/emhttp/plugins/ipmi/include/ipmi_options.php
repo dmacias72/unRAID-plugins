dcd8e4237b128b90e1af911e022e1f9f8a42db39
dmacias72 <derekmacias@live.com>
2016-05-09 22:27:06 -0600

    change ipmi detections

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php
@@ -13,11 +13,10 @@ $password = isset($cfg['PASSWORD']) ? $cfg['PASSWORD'] : '';
 
 /* check if local ipmi driver is loaded */
 if($netsvc == 'disable')
-    $mod = (trim(shell_exec("modprobe ipmi_si --first-time 2>&1 | grep -q 'Module already in kernel' && echo 1 || echo 0 2> /dev/null")) == 1);
-///dev/ipmi0  /dev/ipmi/0  /dev/ipmidev/0
+    $mod = (is_file('/dev/ipmi0') || is_file('/dev/ipmi/0') || is_file('/dev/ipmidev/0'));
+    //$mod = (trim(shell_exec("modprobe ipmi_si --first-time 2>&1 | grep -q 'Module already in kernel' && echo 1 || echo 0 2> /dev/null")) == 1);
+
 /* options for network access */
 $netopts = ($netsvc == 'enable') ? "--always-prefix -h $ipaddr -u $user -p ".
-    base64_decode($password)." --session-timeout=5000 --retransmission-timeout=1000" : '';
-$fanopts = ($netsvc == 'enable') ? "-h $fanip -u $user -p ".
     base64_decode($password)." --session-timeout=5000 --retransmission-timeout=1000" : '';
 ?>
