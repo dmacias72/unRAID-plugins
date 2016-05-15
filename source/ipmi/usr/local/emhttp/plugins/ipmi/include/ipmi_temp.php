81cdcb623b186ab53ae5d85ba1752a9611ea3d1c
dmacias72 <derekmacias@live.com>
2016-05-15 02:44:56 -0600

    add colors to sensor display based on state

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_temp.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_temp.php
@@ -1,31 +1,52 @@
 <?
-require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php';
-require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_settings_display.php';
 require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_helpers.php';
+require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_settings_display.php';
 
 function format_ipmi_temp($reading, $unit, $dot) {
-  return (($reading == 0) ? ($unit=='F' ? round(9/5*$reading+32) : str_replace('.',$dot,$reading)) : '##')."<small>&deg;$unit</small>";
+  return (($reading != 0) ? ($unit=='F' ? round(9/5*$reading+32) : str_replace('.',$dot,$reading))."<small>&deg;$unit</small>" : '##');
 }
 
 $disp_sensors = [$disp_sensor1, $disp_sensor2, $disp_sensor3, $disp_sensor4];
 
 if (!empty($disp_sensors)){
     if(($mod) || ($netsvc == 'enable'))
-        $readings = ipmi_get_readings();
+        $readings = ipmi_sensors();
     $displays = [];
     foreach($disp_sensors as $disp_sensor){
         if ($readings[$disp_sensor]){
             $disp_name    = $readings[$disp_sensor]['Name'];
             $disp_id      = $readings[$disp_sensor]['ID'];
             $disp_reading = $readings[$disp_sensor]['Reading'];
-            $displays[]   = ($readings[$disp_sensor]['Type'] == 'Temperature') ? 
-            "<img src='/plugins/ipmi/icons/cpu.png' title='$disp_name ($disp_id)' class='icon'>"
-                .format_ipmi_temp(floatval($disp_reading), $_GET['unit'], $_GET['dot']): 
-            "<img src='/plugins/ipmi/icons/fan.png' title='$disp_name ($disp_id)' class='icon'>"
-                .floatval($disp_reading)."<small>&thinsp;rpm</small>";
+            $LowerNR = floatval($readings[$disp_sensor]['LowerNR']);
+            $LowerC  = floatval($readings[$disp_sensor]['LowerC']);
+            $LowerNC = floatval($readings[$disp_sensor]['LowerNC']);
+            $UpperNC = floatval($readings[$disp_sensor]['UpperNC']);
+            $UpperC  = floatval($readings[$disp_sensor]['UpperC']);
+            $UpperNR = floatval($readings[$disp_sensor]['UpperNR']);
+            $Color = ($disp_reading == 'N/A') ? 'blue' : 'green';
+
+            if($readings[$disp_sensor]['Type'] == 'Temperature'){
+                // if temperature is greater than upper non-critical show critical
+                if ($disp_reading > $UpperNC)
+                    $Color = 'orange';
+
+                    // if temperature is greater than upper critical show non-recoverable
+                if ($disp_reading > $UpperC)
+                    $Color = 'red';
+
+                $displays[] = "<img src='/plugins/ipmi/icons/cpu.png' title='$disp_name ($disp_id)' class='icon'><font color='$Color'>".
+                    format_ipmi_temp(floatval($disp_reading), $_GET['unit'], $_GET['dot']).'</font>';
+            }elseif($readings[$disp_sensor]['Type'] == 'Fan'){
+                // if Fan RPMs are less than lower non-critical
+                if ($disp_reading < $LowerNC || $disp_reading < $LowerC || $disp_reading < $LowerNR)
+                    $Color = "red";
+
+                $displays[] = "<img src='/plugins/ipmi/icons/fan.png' title='$disp_name ($disp_id)' class='icon'><font color='$Color'>".
+                    floatval($disp_reading)."</font><small>&thinsp;rpm</small>";
+            }
         }
     }
 }
 if ($displays)
-    echo "<span id='temps' style='margin-right:16px'>".implode('&nbsp;', $displays)."</span>";
+    echo "<span id='temps' style='margin-right:16px;font-weight: bold;'>".implode('&nbsp;', $displays)."</span>";
 ?>
