fedd039bdee988bda871c25ec8fc7f8d920d8e43
dmacias72 <derekmacias@live.com>
2016-05-15 02:40:08 -0600

    move ipmi check to helper functions

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_settings.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_settings.php
@@ -1,15 +1,25 @@
 <?
-require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php';
+require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_helpers.php';
 require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_settings_display.php';
 require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_settings_fan.php';
-require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_helpers.php';
 
 /* ipmi settings variables*/
 $seld     = isset($cfg['IPMISELD']) ? $cfg['IPMISELD'] : 'disable';
-$seldpoll = isset($cfg['IPMIPOLL']) ? $cfg['IPMIPOLL'] : "60";
+$seldpoll = isset($cfg['IPMIPOLL']) ? $cfg['IPMIPOLL'] : '60';
 $local    = isset($cfg['LOCAL'])    ? $cfg['LOCAL']    : 'disable';
 
-//check running status
+// create a lockfile for ipmi dashboard
+$dashfile = "$plg_path/ipmidash";
+$dashlock = (file_exists($dashfile));
+if(empty($dashtypes)){
+    if ($dashlock)
+        unlink($dashfile);
+}else {
+    if (!$dashlock)
+        file_put_contents($dashfile, '');
+}
+
+// check running status
 $seld_run    = (trim(shell_exec( "[ -f /proc/`cat /var/run/ipmiseld.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" )) == 1);
 $fanctrl_run = (trim(shell_exec( "[ -f /proc/`cat /var/run/ipmifan.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" )) == 1);
 $running     = "<span class='green'>Running</span>";
@@ -18,16 +28,9 @@ $seld_status    = ($seld_run)    ? $running : $stopped;
 $fanctrl_status = ($fanctrl_run) ? $running : $stopped;
 
 /* get sensor and board info */
-$sensors = [];
-$fantemp = [];
-$board = 'unknown';
-
-if ($mod || ($netsvc == 'enable')){
-    $sensors = ipmi_sensors();
-    $fantemp = ipmi_get_fantemp();
-    $board   = trim(shell_exec("ipmi-fru $netopts | grep 'Board Manufacturer' | awk -F 'r:' '{print $2}'")); // motherboard
-}
-
+$sensors = ipmi_sensors();
+$fantemp = ipmi_get_fantemp();
+$board   = ($ipmi || !empty($netopts)) ? trim(shell_exec("ipmi-fru $netopts | grep 'Board Manufacturer' | awk -F 'r:' '{print $2}'")) : 'unknown';
 $boards_repo = 'https://raw.githubusercontent.com/dmacias72/unRAID-plugins/master/plugins/boards.json';
 $boards_file = "$plg_path/boards.json";
 
@@ -38,6 +41,6 @@ $boards       = json_decode(file_get_contents($boards_file), true);
 $board_status = array_key_exists($board, $boards);
 
 /* check connection */
-if ($netsvc == 'enable')
-    $conn = (!empty($sensors)) ? "Connection successful" : "Connection failed";
+if (!empty($netopts))
+    $conn = (empty($sensors)) ? 'Connection failed' : 'Connection successful';
 ?>
