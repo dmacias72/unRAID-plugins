22895e84b7a0c6a64ef67c6314ecde31e8512002
dmacias72 <derekmacias@live.com>
2016-05-21 10:49:11 -0600

    fix scheduler tab count

--- source/speedtest/usr/local/emhttp/plugins/speedtest/include/parse_cfg.php
+++ source/speedtest/usr/local/emhttp/plugins/speedtest/include/parse_cfg.php
@@ -14,7 +14,7 @@ if (!file_exists($speedtest_filename)) {
 }
 
 // determine the number of tabs for scheduler
-$scheduler_tab = 3;
+$scheduler_tab = (($var['shareUser']!='-')&&(isset($disks['cache']))&&($disks['cache']['status']!='DISK_NP')&&($var['shareCacheEnabled']=='yes')) ? 3 : 2;
 if(file_exists('/boot/config/plugins/dynamix.schedules.plg'))
    $scheduler_tab++;
 if(file_exists('/boot/config/plugins/dynamix.smart.drivedb.plg'))
