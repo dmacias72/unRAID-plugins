d48b0c1295a547cc240a465d003e7d5f59c0916d
dmacias72 <derekmacias@live.com>
2016-05-21 23:15:29 -0600

    add extra speedtest scripts

--- source/speedtest/install/doinst.sh
+++ source/speedtest/install/doinst.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-xml /usr/sbin/speedtest-xml
 chmod +0755 /usr/sbin/speedtest-xml
+ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-linux /usr/sbin/speedtest-linux
+chmod +0755 /usr/sbin/speedtest-linux
+ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-cli /usr/sbin/speedtest-cli
+chmod +0755 /usr/sbin/speedtest-cli
