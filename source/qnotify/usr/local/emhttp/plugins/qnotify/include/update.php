3609c0e3be335caef08a5283ab3fd996a6f6498e
dmacias72 <derekmacias@live.com>
2016-05-22 00:58:28 -0600

    fix save when running

--- source/qnotify/usr/local/emhttp/plugins/qnotify/include/update.php
+++ source/qnotify/usr/local/emhttp/plugins/qnotify/include/update.php
@@ -1,27 +1,29 @@
 <?
-$pcfg      = '/boot/config/plugins/qnotify/config.py';
+if(!$_POST['RUNNING']) {
+    $pcfg     = '/boot/config/plugins/qnotify/config.py';
+    $data     = '';
+    $port     = $_POST['port'];
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+    $token    = trim($_POST['pushbulletAccessToken']);
+    $device   = trim($_POST['pushbulletDeviceName']);
+    $keyword  = implode(',', preg_split('/[\ \,]+/', $_POST['pushIfKeyword'], -1, PREG_SPLIT_NO_EMPTY));
+
+    $data = "host = \"localhost\"\n".
+            "port = $port\n".
+            "username = \"$username\"\n".
+            "password = \"$password\"\n".
+            "enabledPlugins = [\"pushbullet\"]\n".
+            "pushbulletAccessToken = \"$token\"\n".
+            "pushbulletDeviceName = \"$device\"\n".
+            "pushIfKeyword = [\"$keyword\"]\n";
+
+    file_put_contents($pcfg, $data);
+}
+
 $cfg      = '/boot/config/plugins/qnotify/qnotify.cfg';
-$data     = '';
-$port     = $_POST['port'];
-$username = trim($_POST['username']);
-$password = trim($_POST['password']);
-$token    = trim($_POST['pushbulletAccessToken']);
-$device   = trim($_POST['pushbulletDeviceName']);
-$keyword  = implode(',', preg_split('/[\ \,]+/', $_POST['pushIfKeyword'], -1, PREG_SPLIT_NO_EMPTY));
-$daemon   = $_POST['DAEMON'];
-
-$data = "host = \"localhost\"\n".
-        "port = $port\n".
-        "username = \"$username\"\n".
-        "password = \"$password\"\n".
-        "enabledPlugins = [\"pushbullet\"]\n".
-        "pushbulletAccessToken = \"$token\"\n".
-        "pushbulletDeviceName = \"$device\"\n".
-        "pushIfKeyword = [\"$keyword\"]\n";
-
-file_put_contents($pcfg, $data);
-
-$data = "DAEMON=\"$daemon\"\n";
+$daemon = $_POST['DAEMON'];
+$data   = "DAEMON=\"$daemon\"\n";
 
 file_put_contents($cfg, $data);
 
