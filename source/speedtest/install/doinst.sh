513fea52bf0597fa1bf63536eed02588a4e80d5c
dmacias72 <derekmacias@live.com>
2016-06-03 13:03:22 -0600

    add beta speedtest

--- source/speedtest/install/doinst.sh
+++ source/speedtest/install/doinst.sh
@@ -5,3 +5,5 @@ ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-linux /usr/sbin/spe
 chmod +0755 /usr/sbin/speedtest-linux
 ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-cli /usr/sbin/speedtest-cli
 chmod +0755 /usr/sbin/speedtest-cli
+ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-beta /usr/sbin/speedtest-beta
+chmod +0755 /usr/sbin/speedtest-beta
