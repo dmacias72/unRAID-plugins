27459693b6e0a69d988d8879e4f8752ead27376d
Derek Macias <derekmacias@live.com>
2016-06-04 05:55:51 -0600

    add rc script

--- source/libvirtwol/install/doinst.sh
+++ source/libvirtwol/install/doinst.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-SCRIPT="<?php include_once '/usr/local/emhttp/plugins/libvirtwol/include/libvirtwol.php';?>"
+SCRIPT="<? include_once '/usr/local/emhttp/plugins/libvirtwol/include/libvirtwol.php';?>"
 FILE="/usr/local/emhttp/plugins/dynamix.vm.manager/VMSettings.page"
 
 # Update file permissions of scripts
 chmod +0755 /usr/local/emhttp/plugins/libvirtwol/scripts/* \
-	/usr/local/emhttp/plugins/libvirtwol/event/*
+	/usr/local/emhttp/plugins/libvirtwol/event/* \
+	/etc/rc.d/rc.libvirtwol
 
 # add stop to shutdown script	
 if ! grep "$SCRIPT" $FILE >/dev/null 2>&1
