8ce0f625650cc663101d682a0679c8e5cb69f3dd
dmacias72 <derekmacias@live.com>
2016-09-17 01:17:18 -0600

    update

--- source/apache/install/doinst.sh
+++ source/apache/install/doinst.sh
@@ -2,6 +2,7 @@
 
 # Update file permissions of scripts
 chmod +0755 /usr/local/emhttp/plugins/apache/scripts/* \
+	/usr/local/emhttp/plugins/apache/event/* \
 	/etc/rc.d/rc.apache
 
 if [ ! -L /var/www/htdocs ]; then
@@ -9,6 +10,6 @@ if [ ! -L /var/www/htdocs ]; then
 fi
 ln -sfT /var/www/html /srv/httpd/htdocs
 
-cp -nr /etc/httpd /boot/config/plugins/apache
+cp -nr /usr/local/emhttp/plugins/apache/httpd /boot/config/plugins/apache
 rm -rf /etc/httpd
 ln -sfT /boot/config/plugins/apache/httpd /etc/httpd
