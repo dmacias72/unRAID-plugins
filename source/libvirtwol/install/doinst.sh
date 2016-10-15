152e5d65e1132de802729ae0c5de162813dc5239
dmacias72 <derekmacias@live.com>
2016-10-15 10:34:30 -0600

    formatting

--- source/libvirtwol/install/doinst.sh
+++ source/libvirtwol/install/doinst.sh
@@ -4,12 +4,10 @@ FILE="/usr/local/emhttp/plugins/dynamix.vm.manager/VMSettings.page"
 
 # Update file permissions of scripts
 chmod +0755 /usr/local/emhttp/plugins/libvirtwol/scripts/* \
-	/usr/local/emhttp/plugins/libvirtwol/event/* \
-	/etc/rc.d/rc.libvirtwol
+    /usr/local/emhttp/plugins/libvirtwol/event/* \
+    /etc/rc.d/rc.libvirtwol
 
 # add stop to shutdown script	
 if ! grep "$SCRIPT" $FILE >/dev/null 2>&1
-	then echo -e "\n$SCRIPT" >> $FILE
+    then echo -e "\n$SCRIPT" >> $FILE
 fi
-
-
