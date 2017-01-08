ca05789cd32297d931b4040e4e2ab2fb2a97116e
dmacias72 <derekmacias@live.com>
2017-01-07 22:59:55 -0700

    fix node delete

--- source/speedtest/usr/local/emhttp/plugins/speedtest/include/delete_node.php
+++ source/speedtest/usr/local/emhttp/plugins/speedtest/include/delete_node.php
@@ -1,13 +1,14 @@
 <?
 $file = '/boot/config/plugins/speedtest/speedtest.xml';
 if (file_exists($file)) {
-    if ($_GET['id'] == 'all'){
+    if ($_POST['id'] == 'all'){
         $xml = new SimpleXMLElement("<tests></tests>");
         $xml->asXML($file);
     }else {
-        $name = $_GET['id'];
+        $name = $_POST['id'];
         $xml = simplexml_load_file($file);
-        unset($xml->xpath("test[@name=$name]")[0]->{0});
+        $node = $xml->xpath("test[@name=$name]")[0];
+        unset($node[0]);
         $xml->asXML($file);
     }
 }
