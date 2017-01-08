c69c8ce031f4cb5f01f71be7dc0ca013375e4530
dmacias72 <derekmacias@live.com>
2017-01-07 22:48:20 -0700

    rename symlinks

--- source/speedtest/install/doinst.sh
+++ source/speedtest/install/doinst.sh
@@ -3,7 +3,5 @@ ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-xml /usr/sbin/speed
 chmod +0755 /usr/sbin/speedtest-xml
 ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-linux /usr/sbin/speedtest-linux
 chmod +0755 /usr/sbin/speedtest-linux
-ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-cli /usr/sbin/speedtest-cli
-chmod +0755 /usr/sbin/speedtest-cli
-ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest-beta /usr/sbin/speedtest-beta
-chmod +0755 /usr/sbin/speedtest-beta
+ln -sf /usr/local/emhttp/plugins/speedtest/scripts/speedtest /usr/sbin/speedtest
+chmod +0755 /usr/sbin/speedtest
