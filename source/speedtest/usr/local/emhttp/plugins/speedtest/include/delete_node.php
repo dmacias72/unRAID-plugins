435c60b7d22835d5d533a1c31c381d0a16a61305
dmacias72 <derekmacias@live.com>
2017-01-23 21:54:23 -0700

    add version 1.0.2,  update security

--- source/speedtest/usr/local/emhttp/plugins/speedtest/include/delete_node.php
+++ source/speedtest/usr/local/emhttp/plugins/speedtest/include/delete_node.php
@@ -1,13 +1,13 @@
 <?
 $file = '/boot/config/plugins/speedtest/speedtest.xml';
 if (file_exists($file)) {
-    if ($_POST['id'] == 'all'){
+    $id = $_POST['id'];
+    if ($id == 'all'){
         $xml = new SimpleXMLElement("<tests></tests>");
         $xml->asXML($file);
     }else {
-        $name = $_POST['id'];
         $xml = simplexml_load_file($file);
-        $node = $xml->xpath("test[@name=$name]")[0];
+        $node = $xml->xpath("test[@name=$id]")[0];
         unset($node[0]);
         $xml->asXML($file);
     }
--- source/speedtest/usr/local/emhttp/plugins/speedtest/include/parse_cfg.php
+++ source/speedtest/usr/local/emhttp/plugins/speedtest/include/parse_cfg.php
@@ -1,6 +1,6 @@
 <?
 $speedtest_cfg_file = '/boot/config/plugins/speedtest/speedtest.cfg';
-$speedtest_cfg      = file_exists($speedtest_cfg_file) ?parse_ini_file($speedtest_cfg_file) : [];
+$speedtest_cfg      = file_exists($speedtest_cfg_file) ? parse_ini_file($speedtest_cfg_file) : [];
 $speedtest_filename = '/boot/config/plugins/speedtest/speedtest.xml';
 $speedtest_secure   = isset($speedtest_cfg['SECURE'])  ? $speedtest_cfg['SECURE']  : 'no';
 $speedtest_share    = isset($speedtest_cfg['SHARE'])   ? $speedtest_cfg['SHARE']   : 'share';
@@ -8,7 +8,7 @@ $speedtest_units    = isset($speedtest_cfg['UNITS'])   ? $speedtest_cfg['UNITS']
 $speedtest_server   = isset($speedtest_cfg['SERVER'])  ? $speedtest_cfg['SERVER']  : 'none';
 $speedtest_list     = isset($speedtest_cfg['LIST'])    ? $speedtest_cfg['LIST']    : 'auto';
 $speedtest_timeout  = isset($speedtest_cfg['TIMEOUT']) ? $speedtest_cfg['TIMEOUT'] : '10';
-$speedtest_version  = isset($speedtest_cfg['VERSION']) ? $speedtest_cfg['VERSION'] : 'linux';
+$speedtest_version  = isset($speedtest_cfg['VERSION']) ? $speedtest_cfg['VERSION'] : '1.0.2';
 $speedtest_linux    = trim(shell_exec("/usr/sbin/speedtest-linux -v | awk '{print $3}'"));
 
 if (!file_exists($speedtest_filename)) {
