0dbced0435ecada7a3597c17b68e82f9085ea984
dmacias72 <derekmacias@live.com>
2017-01-28 01:21:18 -0700

    update security for XSS and CSRF

--- source/cmdline/usr/local/emhttp/plugins/cmdline/include/cmdline_config.php
+++ source/cmdline/usr/local/emhttp/plugins/cmdline/include/cmdline_config.php
@@ -1,11 +1,11 @@
 <?
 $cmdline_cfg         = parse_ini_file('/boot/config/plugins/cmdline/cmdline.cfg');
-$shellinabox_ipaddr  = isset($cmdline_cfg['IPADDR'])      ? $cmdline_cfg['IPADDR'] : 'disable';
-$shellinabox_hostip  = isset($var['IPADDR'])              ? $var['IPADDR']         : $eth0['IPADDR:0'];
-$shellinabox_host    = ($shellinabox_ipaddr == 'disable') ? $var['NAME']           : $shellinabox_hostip;
-$shellinabox_ssl     = isset($cmdline_cfg['SSL'])         ? $cmdline_cfg['SSL']    : 'disable';
-$shellinabox_port    = (isset($cmdline_cfg['PORT']) && is_numeric($cmdline_cfg['PORT']) && $cmdline_cfg['PORT'] > 0 && $cmdline_cfg['PORT'] < 65535 ) ? $cmdline_cfg['PORT'] : 4200;
-$shellinabox_running = trim(shell_exec( "[ -f /proc/`cat /var/run/shellinaboxd.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ));
+$shellinabox_ipaddr  = isset($cmdline_cfg['IPADDR'])      ? htmlspecialchars($cmdline_cfg['IPADDR']) : 'disable';
+$shellinabox_hostip  = isset($var['IPADDR'])              ? htmlspecialchars($var['IPADDR'])         : $eth0['IPADDR:0'];
+$shellinabox_host    = ($shellinabox_ipaddr == 'disable') ? htmlspecialchars($var['NAME'])           : $shellinabox_hostip;
+$shellinabox_ssl     = isset($cmdline_cfg['SSL'])         ? htmlspecialchars($cmdline_cfg['SSL'])     : 'disable';
+$shellinabox_port    = (isset($cmdline_cfg['PORT']) && is_numeric($cmdline_cfg['PORT']) && $cmdline_cfg['PORT'] > 0 && $cmdline_cfg['PORT'] < 65535 ) ? intval($cmdline_cfg['PORT']) : 4200;
+$shellinabox_running = (intval(trim(shell_exec( "[ -f /proc/`cat /var/run/shellinaboxd.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ))) === 1);
 $status_running      = '<span class="green">Running</span>';
 $status_stopped      = '<span class="orange">Stopped</span>';
 $shellinabox_status  = ($shellinabox_running) ? $status_running : $status_stopped;
--- source/cmdline/usr/local/emhttp/plugins/cmdline/include/cmdline_settings.php
+++ source/cmdline/usr/local/emhttp/plugins/cmdline/include/cmdline_settings.php
@@ -1,12 +1,12 @@
 <?php
 require_once '/usr/local/emhttp/plugins/cmdline/include/cmdline_config.php';
 
-$shellinabox_service = isset($cmdline_cfg['SERVICE']) ? $cmdline_cfg['SERVICE'] : 'disable';
-$cmdline_screen = isset($cmdline_cfg['SCREEN'])       ? $cmdline_cfg['SCREEN']  : 'disable';
-$cmdline_backup = isset($cmdline_cfg['BACKUP'])       ? $cmdline_cfg['BACKUP']  : 'disable';
-$shellinabox_runas = isset($cmdline_cfg['RUNAS'])     ? $cmdline_cfg['RUNAS']   : 'nobody';
-$shellinabox_cert = isset($cmdline_cfg['CERT'])       ? $cmdline_cfg['CERT']    :  'certificate.pem';
-$shellinabox_version = shell_exec( "/usr/sbin/shellinaboxd --version 2>&1 | grep ShellInABox | sed -e 's/^ShellInABox version //;s/(.*//'" );
+$shellinabox_service = isset($cmdline_cfg['SERVICE']) ? htmlspecialchars($cmdline_cfg['SERVICE']) : 'disable';
+$cmdline_screen      = isset($cmdline_cfg['SCREEN'])  ? htmlspecialchars($cmdline_cfg['SCREEN'])  : 'disable';
+$cmdline_backup      = isset($cmdline_cfg['BACKUP'])  ? htmlspecialchars($cmdline_cfg['BACKUP'])  : 'disable';
+$shellinabox_runas   = isset($cmdline_cfg['RUNAS'])   ? htmlspecialchars($cmdline_cfg['RUNAS'])   : 'nobody';
+$shellinabox_cert    = isset($cmdline_cfg['CERT'])    ? htmlspecialchars($cmdline_cfg['CERT'])    :  'certificate.pem';
+$shellinabox_version = '2.20';//shell_exec( "/usr/sbin/shellinaboxd --version 2>&1 | grep ShellInABox | sed -e 's/^ShellInABox version //;s/(.*//'" );
 $shellinabox_port_status = ($shellinabox_running) ?
     "<a target='_blank' href='".$shellinabox_http."://".$shellinabox_host.":".$shellinabox_port."' title='Click on link then accept security, then /Tools/CommandLineTools will work'><b><font color='green'>$shellinabox_port</font></b></a>":
     "<b><font color='orange';'>$shellinabox_port</font></b>";
