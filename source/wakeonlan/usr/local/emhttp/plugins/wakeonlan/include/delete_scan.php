6ccfad8e29bd1b12aaa6b4fc4da3a75b971a7305
dmacias72 <derekmacias@live.com>
2017-01-29 13:19:18 -0700

    fix blank scan xml causing errors

--- source/wakeonlan/usr/local/emhttp/plugins/wakeonlan/include/delete_scan.php
+++ source/wakeonlan/usr/local/emhttp/plugins/wakeonlan/include/delete_scan.php
@@ -1,6 +1,6 @@
 <?
 $log = '/var/log/wakeonlan/scan.xml';
 if (file_exists($log))
-    file_put_contents($log, '');
-    echo json_encode(true);
+    $xml = new SimpleXMLElement('<nmaprun></nmaprun>');
+    $xml->asXML($log);
 ?>
