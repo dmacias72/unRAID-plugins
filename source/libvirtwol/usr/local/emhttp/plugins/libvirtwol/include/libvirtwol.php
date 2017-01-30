0919264e9785668a54b3ea4cb64ea3750e0602e2
dmacias72 <derekmacias@live.com>
2017-01-30 00:17:19 -0700

    sanitized, fix form undefined

--- source/libvirtwol/usr/local/emhttp/plugins/libvirtwol/include/libvirtwol.php
+++ source/libvirtwol/usr/local/emhttp/plugins/libvirtwol/include/libvirtwol.php
@@ -1,8 +1,8 @@
 <?
 $config_file        = '/boot/config/domain.cfg';
 $libvirtwol_cfg     = parse_ini_file($config_file);
-$libvirtwol_service = isset($libvirtwol_cfg['WOL']) ? $libvirtwol_cfg['WOL'] : 'disable';
-$libvirtwol_running = trim(shell_exec( "ps ax | grep 'libvirtwol.py' | grep -v grep &>/dev/null && echo 1 || echo 0 2> /dev/null" ));
+$libvirtwol_service = isset($libvirtwol_cfg['WOL']) ? htmlspecialchars($libvirtwol_cfg['WOL']) : 'disable';
+$libvirtwol_running = (intval(trim(shell_exec( "ps ax | grep 'libvirtwol.py' | grep -v grep &>/dev/null && echo 1 || echo 0 2> /dev/null" ))) === 1);
 $status_running     = '<span class="green">Running</span>';
 $status_stopped     = '<span class="orange">Stopped</span>';
 $libvirtwol_status  = ($libvirtwol_running) ? $status_running : $status_stopped;
@@ -10,13 +10,14 @@ $libvirtwol_status  = ($libvirtwol_running) ? $status_running : $status_stopped;
 <div class="advanced">
 <div id="title" style="white-space:normal;"><span><img src="/plugins/libvirtwol/icons/wakeonlan.png" class="icon">Libvirt wake on lan</span>
 <span class="status"> Status: <?=$libvirtwol_status;?></span></div>
-<form name="libvirtwol_settings" method="POST" action="/update.php" target="progressFrame">
+<div id="wolform"></div>
+<div id="wolinput">
 <input type="hidden" name="#file" value="/boot/config/domain.cfg" />
-<input type="hidden" id="command" name="#command" value="" />
+<input type="hidden" id="wolcommand" name="#command" value="" />
 <dl>
     <dt>Enable Wake On Lan:</dt>
     <dd>
-        <select id="WOL" name="WOL" size="1" onChange="checkRUNNING(this.form);">
+        <select id="WOL" name="WOL" size="1">
             <?=mk_option($libvirtwol_service, "disable", "No");?>
             <?=mk_option($libvirtwol_service, "enable", "Yes");?>
         </select>
@@ -28,35 +29,43 @@ $libvirtwol_status  = ($libvirtwol_running) ? $status_running : $status_stopped;
 </blockquote>
 <dl>
     <dt>&nbsp;</dt>
-    <dd><input id="btnApply" type="submit" value="Apply" onClick="verifyDATA(this.form)"><input type="button" value="Done" onClick="done()"></dd>
+    <dd><input id="btnApply" type="button" value="Apply"><input type="button" value="Done" onClick="done()"></dd>
 </dl>
-</form>
 </div>
-<script type="text/javascript">
+</div>
+<script>
 $(function(){
     // dynamix plugin update api
     <?if (function_exists('plugin_update_available') && $version = plugin_update_available('libvirtwol')):?>
-        showNotice('Wake on Lan <?=$version?> available. <a>Update</a>','libvirtwol');
+        showNotice('Wake on Lan <?=htmlspecialchars($version);?> available. <a>Update</a>','libvirtwol');
         $('#user-notice a').on('click', function () {
             $('#user-notice').empty();
         });
     <?endif;?>
 
-    checkRUNNING(document.libvirtwol_settings);
+    checkRUNNING();
+    $('#WOL').on('change', checkRUNNING);
+    $('#btnApply').on('click', verifyDATA);
+    $('#wolform').html('<form id="wolsettings" name="wolsettings" method="POST" action="/update.php" target="progressFrame"></form>');
+    $('#wolinput').appendTo('#wolsettings');
 });
 
-function checkRUNNING(form) {
-    if ("<?=$libvirtwol_running;?>" == "yes")
-        form.btnApply.disabled = "disabled";
-    if (form.WOL.value == "enable")
-        form.command.value = "/usr/local/emhttp/plugins/libvirtwol/scripts/start";
+function checkRUNNING() {
+    if ($('#WOL').val() === 'enable')
+        $('#wolcommand').val('/usr/local/emhttp/plugins/libvirtwol/scripts/start');
     else {
-        form.command.value = "/usr/local/emhttp/plugins/libvirtwol/scripts/stop";
-        form.btnApply.disabled = (form.WOL.value == "enable");
+        $('#wolcommand').val('/usr/local/emhttp/plugins/libvirtwol/scripts/stop');
+        $('#btnApply').prop('disabled', false);
     }
-};
 
-function verifyDATA(form) {
-        form.WOL.value = form.WOL.value.replace(/ /g,"_");
-};
+    if ("<?=$libvirtwol_running;?>" == true)
+        $('#btnApply').disabled = 'disabled';
+    else
+        $('#btnApply').prop('disabled', false);
+}
+
+function verifyDATA() {
+        $('#WOL').val( $('#WOL').val().replace(/ /g,"_") );
+        $('#wolsettings').submit();
+}
 </script>
