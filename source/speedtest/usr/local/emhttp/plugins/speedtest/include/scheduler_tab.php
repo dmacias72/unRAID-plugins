2f3a49ae815da931002305fad359f960f7897256
dmacias72 <derekmacias@live.com>
2017-02-12 11:16:33 -0700

    calculate tabs based on page file search

--- source/speedtest/usr/local/emhttp/plugins/speedtest/include/scheduler_tab.php
+++ source/speedtest/usr/local/emhttp/plugins/speedtest/include/scheduler_tab.php
@@ -1,8 +1,7 @@
 <?
 // determine the number of tabs for scheduler
-$scheduler_tab = (($var['shareUser']!='-')&&(isset($disks['cache']))&&($disks['cache']['status']!='DISK_NP')&&($var['shareCacheEnabled']=='yes')) ? 3 : 2;
-if(file_exists('/boot/config/plugins/dynamix.schedules.plg'))
-   $scheduler_tab++;
-if(file_exists('/boot/config/plugins/dynamix.smart.drivedb.plg'))
-   $scheduler_tab++;
+$scheduler_tab = (($var['shareUser']!='-')&&(isset($disks['cache']))&&($disks['cache']['status']!='DISK_NP')&&($var['shareCacheEnabled']=='yes')) ? 1 : 0;
+exec("find /usr/local/emhttp/* -type f -maxdepth 2 | xargs grep 'Menu=\"Scheduler' -sl | xargs -L 1 basename", sort($output));
+sort($output);
+$scheduler_tab = $scheduler_tab + array_search('SpeedtestSchedule.page', $output);
 ?>
