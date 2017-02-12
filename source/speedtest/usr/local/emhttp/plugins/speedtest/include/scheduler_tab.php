f3a16134c10fbd7248a627b4cbb1974fa2efa6e5
dmacias72 <derekmacias@live.com>
2017-02-12 11:23:03 -0700

    remove sort

--- source/speedtest/usr/local/emhttp/plugins/speedtest/include/scheduler_tab.php
+++ source/speedtest/usr/local/emhttp/plugins/speedtest/include/scheduler_tab.php
@@ -1,7 +1,7 @@
 <?
 // determine the number of tabs for scheduler
 $scheduler_tab = (($var['shareUser']!='-')&&(isset($disks['cache']))&&($disks['cache']['status']!='DISK_NP')&&($var['shareCacheEnabled']=='yes')) ? 1 : 0;
-exec("find /usr/local/emhttp/* -type f -maxdepth 2 | xargs grep 'Menu=\"Scheduler' -sl | xargs -L 1 basename", sort($output));
+exec("find /usr/local/emhttp/* -type f -maxdepth 2 | xargs grep 'Menu=\"Scheduler' -sl | xargs -L 1 basename", $output);
 sort($output);
 $scheduler_tab = $scheduler_tab + array_search('SpeedtestSchedule.page', $output);
 ?>
