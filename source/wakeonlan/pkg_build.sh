1fba377569a9cbafce9f1ab5f43d7032b4ff2b34
Derek Macias <derekmacias@live.com>
2017-04-08 23:54:32 -0600

    update

--- source/wakeonlan/pkg_build.sh
+++ source/wakeonlan/pkg_build.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 DIR="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
-tmpdir=/tmp/tmp.$(( $RANDOM * 19318203981230 + 40 ))
-plugin=$(basename ${DIR})
-archive="$(dirname $(dirname ${DIR}))/archive"
-version=$(date +"%Y.%m.%d")
-package="${archive}/${plugin}-${version}-x86_64-1.txz"
-md5="${archive}/${plugin}-${version}-x86_64-1.md5"
+TMPDIR=/tmp/tmp.$(( $RANDOM * 19318203981230 + 40 ))
+PLUGIN=$(basename ${DIR})
+ARCHIVE="$(dirname $(dirname ${DIR}))/archive"
+DESTDIR="$TMPDIR/usr/local/emhttp/plugins/${PLUGIN}"
+PLG_FILE="$(dirname $(dirname ${DIR}))/plugins/${PLUGIN}.plg"
+VERSION=$(date +"%Y.%m.%d")
+PACKAGE="${ARCHIVE}/${PLUGIN}-%s.txz"
+MD5="${ARCHIVE}/${PLUGIN}-%s.md5"
 
-if [[ -f $package ]]; then
-  for x in a b c d e d f g h ; do
-    package="${archive}/${plugin}-${version}${x}-x86_64-1.txz"
-    md5="${archive}/${plugin}-${version}${x}-x86_64-1.md5"
-    if [[ ! -f $package ]]; then
-      break
-    fi
-  done
-fi
+for x in '' a b c d e d f g h ; do
+  pkg=$(printf "$PACKAGE" "${VERSION}${x}")
+  if [[ ! -f "$pkg" ]]; then
+    PACKAGE=$pkg
+    MD5=$(printf "$MD5" "${VERSION}${x}")
+    VERSION="${VERSION}${x}"
+    break
+  fi
+done
 
-mkdir -p $tmpdir
+sed -i -e "s#\(ENTITY\s*version[^\"]*\).*#\1\"${VERSION}\">#" "$PLG_FILE"
+
+mkdir -p "${DESTDIR}/"
 cd "$DIR"
-cp --parents -f $(find . -type f ! \( -iname "pkg_build.sh" -o -iname "sftp-config.json" -o -iname ".DS_Store"  \) ) $tmpdir/
-cd "$tmpdir/"
-makepkg -l y -c y "${package}"
-cd "$archive/"
-md5sum $(basename "$package") > "$md5"
-rm -rf "$tmpdir"
+cp --parents -f $(find . -type f ! \( -iname "pkg_build.sh" -o -iname "sftp-config.json" -o -iname ".DS_Store"  \) ) "${DESTDIR}/"
+cd "$TMPDIR/"
+makepkg -l y -c y "${PACKAGE}"
+cd "$ARCHIVE/"
+md5sum $(basename "$PACKAGE") > "$MD5"
+rm -rf "$TMPDIR"
 
 # Verify and install plugin package
-sum1=$(md5sum "${package}")
-sum2=$(cat "$md5")
+sum1=$(md5sum "${PACKAGE}")
+sum2=$(cat "$MD5")
 if [ "${sum1:0:32}" != "${sum2:0:32}" ]; then
   echo "Checksum mismatched.";
-  rm "$md5" "${package}"
+  rm "$MD5" "${PACKAGE}"
 else
   echo "Checksum matched."
 fi
--- source/wakeonlan/usr/local/emhttp/plugins/wakeonlan/include/wol_settings.php
+++ source/wakeonlan/usr/local/emhttp/plugins/wakeonlan/include/wol_settings.php
@@ -1,7 +1,10 @@
 <?
-$net_cfg   = parse_ini_file('/boot/config/network.cfg');
-$excludeip = is_array($net_cfg['IPADDR']) ? $net_cfg['IPADDR'][0] : $net_cfg['IPADDR'];
+if ($var['version']  < 6.2)
+    $excludeip = $var['IPADDR'];
+else
+    $excludeip = empty($eth0['IPADDR:0']) ? $eth1['IPADDR:0'] : $eth0['IPADDR:0'];
+
 $ifname    = (filter_var($excludeip, FILTER_VALIDATE_IP)) ?
-    exec("ifconfig | awk 'BEGIN { ifname = 0 } /".escapeshellarg($excludeip)."/ { ifname = 1 } /^($|[^\t])/ { if(ifname) print buffer; buffer = $1; ifname = 0 } END { if(ifname) print buffer }' |  sed s/\:/\/g"): 
+    exec("ifconfig | awk 'BEGIN { ifname = 0 } /".escapeshellarg($excludeip)."/ { ifname = 1 } /^($|[^\t])/ { if(ifname) print buffer; buffer = $1; ifname = 0 } END { if(ifname) print buffer }' |  sed s/\:/\/g"):
     'eth0';
 ?>
